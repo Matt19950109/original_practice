1f504883d47a78dbaa67a273398b76d119040f42
Matt19950109 <136223566+Matt19950109@users.noreply.github.com>
2023-09-27 12:30:12 +0900

    spendingコントローラーの設定

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -16,6 +16,26 @@ class SpendingsController < ApplicationController
     end
   end
 
+  def destroy
+    spending = Spending.find(params[:id])
+    spending.destroy
+    redirect_to spendings_path
+  end
+
+  def edit
+    @spending = Spending.find(params[:id])
+  end
+
+  def update
+    spending = Spending.find(params[:id])
+    spending.update(spending_params)
+    redirect_to spending_path
+  end
+
+  def show
+    @spending = Spending.find(params[:id])
+  end
+
   private
   def spending_params
     params.require(:spending).permit(:price, :item_name, :settlement_id, :start_time)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'incomes#index'
   resources :incomes, only: [:index, :new, :create]
   resources :settlements, only: [:index, :new, :create]
-  resources :spendings , only: [:index, :new, :create]
+  resources :spendings
 end
